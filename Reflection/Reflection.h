99d8bd793d0e8f25750a97d3e9c9a99582b8394f
Nikita Soldatov <acacuce2@gmail.com>
2017-07-03 17:00:12 +0300

    Add carthage compatibility

---
+++ Reflection/Reflection.h
@@ -0,0 +1,19 @@
+//
+//  Reflection.h
+//  Reflection
+//
+//  Created by Никита Солдатов on 03.07.17.
+//  Copyright © 2017 imc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Reflection.
+FOUNDATION_EXPORT double ReflectionVersionNumber;
+
+//! Project version string for Reflection.
+FOUNDATION_EXPORT const unsigned char ReflectionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Reflection/PublicHeader.h>
+
+
